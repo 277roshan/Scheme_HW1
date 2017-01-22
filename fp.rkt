2e9621a16af6f476ddfa1c3fa726047684dbc589
Roshan Thapaliya <gorkhali@Roshans-MacBook-Pro.local>
2017-01-22 14:40:04 -0500

    Testing all the cases available and passed

--- fp.rkt
+++ fp.rkt
@@ -4,7 +4,7 @@
 
 ;define the function to reverse the list named reverse_general
 ;parameter: L (a list)
-(DEFINE (reverse_general L)
+(DEFINE (reverse-general L)
          (COND
             ;check when L is empty then just return L  
             ((NULL? L)
@@ -12,11 +12,31 @@
             )
             ;if the first value in the list is a list then apply reverse to that list as well
             ((LIST? (CAR L))
-                (append (reverse_general (CDR L)) (list (reverse_general (CAR L))))
+                (append (reverse-general (CDR L)) (list (reverse-general (CAR L))))
             )
             ;if the CAR value is not list then we can append it and recurse on remaining list
             (ELSE
-                 (append  (reverse_general (CDR L)) (list (CAR L)) )
+                 (append  (reverse-general (CDR L)) (list (CAR L)))
             )
         )  
 )
+
+
+;testing the cases
+(reverse-general '())
+(reverse-general '(a b c))
+(reverse-general '(a b ()))
+(reverse-general '((a b c)))
+(reverse-general '((a b c) (d e f)))
+(reverse-general '(a (b c) ((d e) f) g))
+(reverse-general '(1 (2 3) (4 (a (b (c d))))))
+
+
+;()
+;(c b a)
+;(() b a)
+;((c b a))
+;((f e d) (c b a))
+;(g (f (e d)) (c b) a)
+;(((((d c) b) a) 4) (3 2) 1)
+
