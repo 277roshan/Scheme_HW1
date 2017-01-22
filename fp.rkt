647a577ced147688020501c54b2d07eca617d2e1
Roshan Thapaliya <gorkhali@Roshans-MacBook-Pro.local>
2017-01-22 14:31:43 -0500

    if the value we get is not list we can append it and recurse on the remaining list

--- fp.rkt
+++ fp.rkt
@@ -12,9 +12,11 @@
             )
             ;if the first value in the list is a list then apply reverse to that list as well
             ((LIST? (CAR L))
-              
-
                 (append (reverse_general (CDR L)) (list (reverse_general (CAR L))))
+            )
+            ;if the CAR value is not list then we can append it and recurse on remaining list
+            (ELSE
+                 (append  (reverse_general (CDR L)) (list (CAR L)) )
             )
         )  
 )
