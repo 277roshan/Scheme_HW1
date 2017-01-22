4d11a3f5edd8fdf9e4118f2498c4f84b5920d615
Roshan Thapaliya <gorkhali@Roshans-MacBook-Pro.local>
2017-01-22 13:55:05 -0500

    housekeeping

--- fp.rkt
+++ fp.rkt
@@ -5,7 +5,10 @@
 ;define the function to reverse the list named reverse_general
 ;parameter: L (a list)
 (DEFINE (reverse_general L)
-
-
-
+         (COND
+            ;check when L is empty then just return L  
+            ((NULL? L)
+                L
+            )
+        )  
 )
