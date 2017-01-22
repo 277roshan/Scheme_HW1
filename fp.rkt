1757f695ebf154786f96e7f293d6686055832561
Roshan Thapaliya <gorkhali@Roshans-MacBook-Pro.local>
2017-01-22 15:48:08 -0500

    in other cases just add 0 to the list and recurse

--- fp.rkt
+++ fp.rkt
@@ -29,7 +29,7 @@
 (DEFINE(sum-up-numbers-simple L)
 
        (COND
-           ;if list is null then just return ) to be added to the sum 
+           ;if list is null then just return 0 to be added to the sum 
            ((NULL? L)
                 0
             )
@@ -41,6 +41,10 @@
            ;if value we get from car is a number then add that value and recurse on rest of the list
            ((NUMBER? (CAR L))
                (+ (CAR L) (sum-up-numbers-simple (CDR L)))
+           )
+           ;in other cases just add 0 and recurse on rest of the list
+           (ELSE
+               (+ 0 (sum-up-numbers-simple (CDR L)))
            )
 
          
